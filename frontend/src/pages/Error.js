c8fa2e5ebe08b2fb01e3152957475ba003898c97
Jick Suhoff <jicksuhoff@gmail.com>
2023-10-04 03:16:51 +0300

    commit 04.10

---
+++ frontend/src/pages/Error.js
@@ -0,0 +1,32 @@
+import { useRouteError } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
+
+function ErrorPage() {
+
+    const error = useRouteError();
+
+    let title = 'An Error occured';
+    let message = 'Something went wrong';
+
+    if (error.status === 500) {
+        message = error.data.message;
+    }
+
+    if (error.status === 404) {
+        title = 'Not found'
+        message = 'Could not find resourse or page'
+    }
+
+    return (
+        <>
+            <MainNavigation />
+            <PageContent title={title} >
+                <p>{message}</p>
+            </PageContent>
+        </>
+    )
+};
+
+export default ErrorPage;
